5b233135ed4af79ff16a734305e5480b0332ee39
Dave <developer31@sinergiaonline.com>
2026-06-17 13:57:29 -0500

    add(gym): GymRepository con validación por nombre

--- src/main/java/com/gymbro/core/repository/GymRepository.java
+++ src/main/java/com/gymbro/core/repository/GymRepository.java
@@ -4,4 +4,7 @@ import com.gymbro.core.entity.Gym;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GymRepository extends JpaRepository<Gym, Long> {
+
+    boolean existsByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCaseAndIdNot(String name, Long id);
 }
